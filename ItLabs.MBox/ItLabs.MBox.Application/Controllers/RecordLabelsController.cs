1e02b306ce648d415fde6bd428842d7096e615fd
danilopopovikj <danilo.popovikj@hotmail.com>
2018-09-18 13:02:42 +0200

    Multiple artists base setup

--- ItLabs.MBox/ItLabs.MBox.Application/Controllers/RecordLabelsController.cs
+++ ItLabs.MBox/ItLabs.MBox.Application/Controllers/RecordLabelsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ItLabs.MBox.Application.Models;
@@ -22,8 +23,8 @@ namespace ItLabs.MBox.Application.Controllers
         private readonly IEmailsManager _emailsManager;
         private readonly IRecordLabelManager _recordLabelManager;
 
-        public RecordLabelsController(IArtistManager artistsManager, MBoxUserManager userManager,IEmailsManager emailsManager, IRecordLabelManager recordLabelManager)
-        {      
+        public RecordLabelsController(IArtistManager artistsManager, MBoxUserManager userManager, IEmailsManager emailsManager, IRecordLabelManager recordLabelManager)
+        {
             _artistsManager = artistsManager;
             _userManager = userManager;
             _emailsManager = emailsManager;
@@ -32,10 +33,10 @@ namespace ItLabs.MBox.Application.Controllers
 
         public IActionResult Index()
         {
-           var recordLabelId = System.Convert.ToInt32(_userManager.GetUserId(HttpContext.User));         
+            var recordLabelId = System.Convert.ToInt32(_userManager.GetUserId(HttpContext.User));
 
-           DashboardViewModel model = new DashboardViewModel() { RecordLabelId = recordLabelId, Skip = 0, Take = 20 };
-           model.PagingList = _artistsManager.GetRecordLabelArtists(model.RecordLabelId, model.Skip, model.Take);
+            DashboardViewModel model = new DashboardViewModel() { RecordLabelId = recordLabelId, Skip = 0, Take = 20 };
+            model.PagingList = _artistsManager.GetRecordLabelArtists(model.RecordLabelId, model.Skip, model.Take);
 
             return View(model);
         }
@@ -61,8 +62,8 @@ namespace ItLabs.MBox.Application.Controllers
             if (!ModelState.IsValid)
                 return View(model);
             var recordLabelId = Convert.ToInt32(_userManager.GetUserId(HttpContext.User));
-            var recordLabel = _recordLabelManager.GetOne(filter: x=>x.Id == recordLabelId, includeProperties: $"{nameof(User)}");
-            if(recordLabel == null)
+            var recordLabel = _recordLabelManager.GetOne(filter: x => x.Id == recordLabelId, includeProperties: $"{nameof(User)}");
+            if (recordLabel == null)
                 return View("AddNewArtist", model);
 
             var user = _userManager.CreateUser(model.Name, model.Email, Role.Artist).Result;
@@ -73,9 +74,9 @@ namespace ItLabs.MBox.Application.Controllers
                 return View("AddNewArtist", model);
             }
 
-            _artistsManager.Create(new Artist { User= user }, recordLabelId);
+            _artistsManager.Create(new Artist { User = user }, recordLabelId);
             _artistsManager.Save();
-            var artist = _artistsManager.GetOne(filter: x => x.User == user, includeProperties:$"{nameof(User)}");
+            var artist = _artistsManager.GetOne(filter: x => x.User == user, includeProperties: $"{nameof(User)}");
             _artistsManager.AddArtistToRecordLabel(artist, recordLabel);
             var code = await _userManager.GeneratePasswordResetTokenAsync(user);
             var callbackUrl = Url.ResetPasswordCallbackLink(artist.Id.ToString(), code, Request.Scheme);
@@ -109,6 +110,28 @@ namespace ItLabs.MBox.Application.Controllers
             model.PagingList = _artistsManager.GetRecordLabelArtists(model.RecordLabelId, model.Skip, model.Take);
             return RedirectToAction("Index", "RecordLabels");
         }
+        [HttpGet]
+        public IActionResult AddMultipleArtists()
+        {
+            return View();
+        }
+        //[HttpPost("UploadCsvFile")]
+        //public async Task<IActionResult> UploadCsvFile(IFormFile file)
+        //{
 
+        //    // full path to file in temp location
+        //    var filePath = Path.GetTempFileName();
+        //    file.
+
+        //    if (file.Length > 0)
+        //    {
+        //        using (var stream = new FileStream(filePath, FileMode.Create))
+        //        {
+
+        //        }
+        //    }
+
+        //    return View("AddMultipleArtists");
+        //}
     }
 }
