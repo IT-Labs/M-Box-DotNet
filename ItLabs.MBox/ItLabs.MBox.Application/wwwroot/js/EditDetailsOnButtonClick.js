95d6310f4cc0580f3bfd32f788002e45c46d53bb
MartinM993 <martinmilutinovic993@gmail.com>
2018-09-18 16:54:06 +0200

    #311296

--- ItLabs.MBox/ItLabs.MBox.Application/wwwroot/js/EditDetailsOnButtonClick.js
+++ ItLabs.MBox/ItLabs.MBox.Application/wwwroot/js/EditDetailsOnButtonClick.js
@@ -1,1 +1,29 @@
-﻿
+﻿window.onload = function () {
+    document.getElementsByClassName('save').style.display = 'none';
+    document.getElementsByClassName('cansel').style.display = 'none';
+    var songName = document.getElementById('songName').value;
+}
+
+function editDetails() {
+    document.getElementById('songName').disabled = false;
+    document.getElementById('saveSongName').style.display = 'block';
+    document.getElementById('cancelSongName').style.display = 'block';
+    document.getElementById('editSongName').style.display = 'none';
+
+}
+
+function saveDetails() {
+    document.getElementById('songName').disabled = true;
+    songName = document.getElementById('songName').value;
+    document.getElementById('editSongName').style.display = 'block';
+    document.getElementById('saveSongName').style.display = 'none';
+    document.getElementById('cancelSongName').style.display = 'none';
+}
+
+function cancelEditiong() {
+    document.getElementById('songName').value = songName;
+    document.getElementById('songName').disabled = true;
+    document.getElementById('editSongName').style.display = 'block';
+    document.getElementById('saveSongName').style.display = 'none';
+    document.getElementById('cancelSongName').style.display = 'none';
+}
