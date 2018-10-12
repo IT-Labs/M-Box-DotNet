271887c73db30030aa0011d7f3fd72b42ef1d0f2
danilopopovikj <danilo.popovikj@hotmail.com>
2018-10-12 14:11:25 +0200

    Search bugs

--- ItLabs.MBox/ItLabs.MBox.Application/wwwroot/js/SearchResultsScript.js
+++ ItLabs.MBox/ItLabs.MBox.Application/wwwroot/js/SearchResultsScript.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     $('input:radio[value=' + $('#selectedButton').val() + ']').prop('checked', true);
 });
-//$('input[type=radio]').on('change', function () {
-//    $(this).closest("form").submit();
-//});
+$('input[type=radio]').on('change', function () {
+    $(this).closest("a").click();
+});
