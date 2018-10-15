12ca6ff3b8b21c9b208099a31b7445700e3f6f62
danilopopovikj <danilo.popovikj@hotmail.com>
2018-10-15 11:55:52 +0200

    Script

--- ItLabs.MBox/ItLabs.MBox.Application/wwwroot/js/SearchResultsScript.js
+++ ItLabs.MBox/ItLabs.MBox.Application/wwwroot/js/SearchResultsScript.js
@@ -2,5 +2,5 @@
     $('input:radio[value=' + $('#selectedButton').val() + ']').prop('checked', true);
 });
 $('input[type=radio]').on('change', function () {
-    $(this).closest("a").click();
+    $(this).closest("a")[0].click();
 });
